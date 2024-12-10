cb1c7b69467ff38af71cded30132f7e62518b684
mari <mariia999@bk.ru>
2024-12-10 19:18:54 +0300

    New  scripts Movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        Vector3 currentPosition = transform.position;
+        currentPosition.x += input.x * _speed * Time.deltaTime;
+        currentPosition.z += input.y * _speed * Time.deltaTime;
+        transform.position = currentPosition;
+    }
+}
